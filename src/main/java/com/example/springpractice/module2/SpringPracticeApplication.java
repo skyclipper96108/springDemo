4dea682e0fce3020074336d149b3358e6972af44
jacob <zhangchujie@inspur.com>
2023-07-06 18:11:08 +0800

    position

--- src/main/java/com/example/springpractice/module2/SpringPracticeApplication.java
+++ src/main/java/com/example/springpractice/module2/SpringPracticeApplication.java
@@ -2,12 +2,16 @@ package com.example.springpractice.module2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})//排除数据源的自动配置
 public class SpringPracticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringPracticeApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(SpringPracticeApplication.class, args);
+        context.close();
     }
 
 }
